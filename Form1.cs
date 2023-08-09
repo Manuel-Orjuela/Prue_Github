d00eb578aae97862f1a2532c4fbef451548dffb4
Sharky-25 <davidbpineda251@gmail.com>
2023-08-09 10:23:27 -0500

    Agregado checkbox

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Prueb_GitHub
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
